0ad46066333b144eb7be13dd4097479a01ed412b
Jchosenn <jonathanchosen79@gmail.com>
2025-09-11 03:31:13 +0100

    worked on theme
    theme is set by system's default theme

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-const icon = document.getElementById('icon')
-const h2Text = document.querySelector('.h2Text')
+// const icon = document.getElementById('icon')
+// const h2Text = document.querySelector('.h2Text')
 
+
+
+// icon.addEventListener('click', () =>{
+//     document.body.classList.toggle('dark')
+//     if(document.body.classList.contains('dark')){
+//         icon.src = 'images/sun-solid-full.svg'
+//         document.body.classList.remove('dark')
+//     }else{
+//         icon.src = 'images/moon-solid-full.svg'
+//         console.log('not correct')
+//     }
+// })
+
+const icon = document.getElementById('icon')
+
+if (localStorage.getItem('theme') == null){
+    localStorage.setItem('theme', 'light')
+}
+
+
+let localData = localStorage.getItem('theme')
+
+if(localData == 'light'){
+    icon.src ="images/moon-solid-full.svg"
+    document.body.classList.remove('dark')
+}else if(localData == 'dark'){
+    icon.src ="images/sun-solid-full.svg"
+    document.body.classList.add('dark')
+}
 
 icon.addEventListener('click', () =>{
     document.body.classList.toggle('dark')
     if(document.body.classList.contains('dark')){
-        icon.src = 'images/sun-solid-full.svg'
+        icon.src = "images/sun-solid-full.svg"
+    localStorage.setItem('theme', 'dark')
     }else{
-        console.log('not correct')
+    icon.src = "images/moon-solid-full.svg"
+    localStorage.setItem('theme', 'light')
     }
-    console.log('testing')
 })
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+  module.exports = {
+      content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+      ],
+      theme: {
+        extend: {
+          colors: {
+            'custom-blue': '#243c5a',
+          },
+        },
+      },
+      plugins: [],
+      theme: "class",
+    };
